2259c54401fd4a88f1d02d4cb1bdb7552c92ad60
onja302 <andriatsitohainaonja@gmail.com>
2020-03-06 09:27:41 +0300

    v1

---
+++ lib/monkey.rb
@@ -0,0 +1,28 @@
+class Monkey
+    @name = name
+    @species = species
+    @foods_eaten = []
+
+    def eat(food)
+
+
+
+    end
+
+    def introduce(name,species,food_eaten)
+
+
+    end
+    
+
+
+
+
+
+
+
+
+
+
+
+end
